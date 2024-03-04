1788104dc232052d8f4c50bf73ef92fe267d905f
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-03-04 22:28:33 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -14,6 +14,26 @@ namespace CMP1903_A1_2324
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+            Game game = new Game();
+            bool done = false;
+            while (!done) 
+            { 
+                game.Play();
+                Console.WriteLine("Do you want to play again");
+                String playAgain = Console.ReadLine();
+                playAgain = playAgain.Trim();
+                playAgain = playAgain.ToLower();
+                if (playAgain != "yes")
+                { 
+                    done = true;
+                }
+            }
+            Console.WriteLine("Thanks for Playing");
+            Testing test = new Testing();
+            test.DieCheck();
+            test.GameCheck();
+            Console.ReadKey();
+
         }
     }
 }
