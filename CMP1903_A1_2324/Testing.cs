f4344e2f4afdd69606be36b1b7147da25bf3414a
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-03-07 10:00:42 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,44 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
     internal class Testing
     {
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
-
+        /// <summary>
+        /// Creating the die and game objects
+        /// </summary>
         Die dice = new Die();
         Game game = new Game();
 
         // Checks the roll methods in the die class to see if returns a number between 1 and 6
         public void DieCheck()
         { 
-            int roll = dice.Roll();
-            Debug.Assert(roll > 0 & roll <7);
+            // Getting the value from the property
+            int roll = dice.Num;
+            // Cehcking if it is between 1 and 6
+            Debug.Assert(roll > 0 & roll < 7);
+            // <black-eye-inc> Review:
+            // Rather than "(roll > 0 & roll < 7)", I'd recommend "roll > 0 && roll < 7" as "&&" is
+            // more accepted as the standard symbols for the C# AND operator.
+
+            // I personally like to add a lot of "Console.WriteLine(...)" comments in my testing
+            // code so that it is very clear what worked and what didn't.
+            // </black-eye-inc>
         }
 
-        // Checks the play method to see if it adds the values correctly
+        /// <summary>
+        /// Checks the play method to see if it adds the values correctly
+        /// </summary>
         public void GameCheck()
         {
-            // int suspectedTotal = game.Play();
-            // int num1 = game.RolledNumber1;
-            // Console.WriteLine(num1);
-            // int num2 = game.RolledNumber2;
-            // Console.WriteLine(num2);
-            // int num3 = game.RolledNumber3;
-            // Console.WriteLine(num3);
-            // int total = num1 + num2 + num3;
-            // Console.WriteLine($"testing {total}");
-            // Debug.Assert(suspectedTotal == total);
+            // The game plays again so I added this so it wasn't confusing for the user when it does
+            Console.WriteLine("Testing");
+            // Getting what the total should be
+            int suspectedTotal = game.Play();
+            // Getting the indivual rolls by the property
+            int num1 = game.RolledNumber1;
+            int num2 = game.RolledNumber2;
+            int num3 = game.RolledNumber3;
+            // Calculating the total
+            int total = num1 + num2 + num3;
+            // Comparing the total to the suspected total
+            Debug.Assert(suspectedTotal == total);
+            // <black-eye-inc>
+            // That's a great job, but the "Additional information" mark criteria, you also need to
+            // make a Debug.Assert to make sure all outputs are within 3 to 18 range.
+            // </black-eye-inc>
         }
     }
 }
