9a5a58479d257dc1d04fc53e921226ee0a747429
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-03-07 09:59:57 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -11,14 +11,18 @@ namespace CMP1903_A1_2324
         {
             // Creating the game object
             Game game = new Game();
-            // Setting a done variable to I can end the while loop
+            // Setting a done variable to true can end the while loop
             bool done = false;
             while (!done) 
             {
                 // Playing the game by calling the method=
                 game.Play();
                 // Asking user to play again
-                Console.WriteLine("Type yes if you want to play again");
+                Console.WriteLine("Type \"yes\" if you want to play again");
+                // <black-eye-inc> Review:
+                // I personally would put "yes" in quotation marks so it is more clear to the user
+                // on exactly what you want them to input
+                // </black-eye-inc>
                 String playAgain = Console.ReadLine();
                 // Two methods to help against type errors but doesn't prevent them
                 // Gets rid of any white spaces before and after the word
@@ -31,9 +35,22 @@ namespace CMP1903_A1_2324
                     // Ending the while loop
                     done = true;
                 }
+                // <black-eye-inc> Review:
+                // I personally would force the user to respond again and again until they give a
+                // valid responce and using a wider array of valid potential answers ("yes"/"y" or
+                // "no"/"n").
+                // So there's plenty of room you could expand this method (potentially by doing
+                // what I did and putting it in its own function with exception handling to show
+                // off the 'exhaustive exception handing'... although that isn't an OOP principle,
+                // so I don't know how many marks it's worth).
+                // </black-eye-inc>
                 // Seperating the games in the termial
                 Console.WriteLine("");
             }
+            // <black-eye-inc>
+            // I thought the EXTRA Functionality "Continuous Roll function" was supposed to be a
+            // method within the Game class.
+            // </black-eye-inc>
             // Finished the loop and displays the message to say that the game is completed 
             Console.WriteLine("Thanks for Playing");
             // // Seperating the games and the testing in the termial
@@ -43,7 +60,7 @@ namespace CMP1903_A1_2324
             // Checking the methods in each class
             test.DieCheck();
             test.GameCheck();
-            Console.ReadKey();
+            Console.ReadLine();//CHANGED FROM "Console.ReadKey();" to "Console.ReadLine();" -- RESET once done reviewing code!
         }
     }
 }
